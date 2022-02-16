f07540486db49e88cd06a4340326130996e7e475
Freddy Abelard <abelard.freddy@gmail.com>
2022-02-16 16:47:38 -0500

    Start the simple air bnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  flat = Flat.create!(
+    name: Faker::JapaneseMedia::OnePiece.location,
+    address: Faker::Address.street_name,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: rand(50..200),
+    number_of_guests: rand(1..6)
+  )
+  puts "Flat #{flat.id} was created"
+end
+puts "All done"
